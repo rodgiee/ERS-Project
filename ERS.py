c99e7342965fb79c705ef7b06d73f4523d29d1ea
Rodge Rebeca <rodgerebeca@Rodges-MacBook-Pro.local>
2025-07-16 14:41:39 -0400

    added: game loop and threading

--- ERS.py
+++ ERS.py
@@ -1,24 +1,27 @@
 import random
+import threading
 def Game():
-    num_players= input("How many players?: ")
-    main_players_name = input("What is your name?: ")
+    num_bots= input("How many bots?: ")
+    name_player = input("What is your name?: ")
 
-    deck = [] # initialize the deck of cards for this game
-    game_players = [] # Initialize the players playing this game
+    global deck_game 
+    deck_game = [] # initialize the deck_game of cards for this game
+
+    players_game = [] # Initialize the players playing this game
     '''
-    Initialize deck of cards,
+    Initialize deck_game of cards,
     Iterate for each suit to create a new value under that suit,
-    Shuffle card objects in deck
+    Shuffle card objects in deck_game
     '''
-    print(f"\nDeck initializing...(currently {len(deck)} in deck)")
+    print(f"\nDeck initializing...(currently {len(deck_game)} in deck_game)")
     for suits in range(4):
         for value in range (1,14):
-            deck.append(Card(suits, value, -1)) # append this new card to the deck list
-    print(f"Deck Initalized! (created {len(deck)} cards in deck)\n")
+            deck_game.append(Card(suits, value, -1)) # append this new card to the deck_game list
+    print(f"Deck Initalized! (created {len(deck_game)} cards in deck_game)\n")
 
-    print(f"Deck shuffling... (first card = {str(deck[0])})")
-    random.shuffle(deck) # shuffle the deck
-    print(f"Deck shuffled! (first card now = {str(deck[0])})\n")
+    print(f"Deck shuffling... (first card = {str(deck_game[0])})")
+    random.shuffle(deck_game) # shuffle the deck_game
+    print(f"Deck shuffled! (first card now = {str(deck_game[0])})\n")
 
     '''
     Create player objects based on desired player count input
@@ -28,48 +31,78 @@ def Game():
     first_names = ["Zonko", "Fizzler", "Snorple", "Quibly", "Norbix", "Jaxor", "Bloopo", "Zindle", "Crunkle", "Vexon"]
     last_names = ["Glimbo", "Torkel", "Zappy", "Marnix", "Flurbo", "Kazzle", "Droopo", "Wibbit", "Tronix", "Blixel"]
     print("Creating players...")
-    game_players.append(Player(0, [], main_players_name)) # Create main player
-    for p in range(1, int(num_players) + 0): # Create bot playeres
-        game_players.append(Player(p, [], random.choice(first_names)+random.choice(last_names)))
+    players_game.append(Player(0, [], name_player)) # Create main player
+    for p in range(1, int(num_bots) + 0): # Create bot playeres
+        players_game.append(Player(p, [], random.choice(first_names)+random.choice(last_names)))
     print("Players created!")
     print("(", end="", flush=True)
-    for p in game_players: print(f"{str(p)}, ", end="", flush=True)
+    for p in players_game: print(f"{str(p)}, ", end="", flush=True)
     print(")", end="", flush=True)
     
     '''
-    Distribute the current game's deck of cards:
-    Start with the 0 player --> take the card at index 0 --> append to player's card inventory while removing it from game deck -->
-    move to next player --> repeat until no more cards in deck
+    Distribute the current game's deck_game of cards:
+    Start with the 0 player --> take the card at index 0 --> append to player's card inventory while removing it from game deck_game -->
+    move to next player --> repeat until no more cards in deck_game
     '''
     print("\n\nDistributing cards to players...")
     current_player_id = 0
-    while(len(deck) > 0):
-        game_players[current_player_id].inventory.append(deck.pop(0))
-        current_player_id = (current_player_id + 1) % len(game_players)
+    while(len(deck_game) > 0):
+        players_game[current_player_id].inventory.append(deck_game.pop(0))
+        current_player_id = (current_player_id + 1) % len(players_game)
     print("Distributed cards to players!")
-    for p in game_players: print(f"({str(p)} has {len(p.inventory)} cards.)")
+    for p in players_game: print(f"({str(p)} has {len(p.inventory)} cards.)")
 
-    is_game_won = False
+    is_game_running = True 
+
     #is_pattern = False
     current_player_id = 0
-    main_player_object = game_players[0]
-    while not (is_game_won):
+
+    # reference to main player object 
+    main_player_object = players_game[0]
+
+    global deck_lock
+    deck_lock = threading.Lock()
+
+    while (is_game_running):
+        main_player = threading.Thread(target=control_player, args=(current_player_id, main_player_object))
+        
         ##check for patterns
-        #top_card_index = len(deck) - 1
+        #top_card_index = len(deck_game) - 1
 #
-        #if (len(deck) >= 2 and (deck[top_card_index] == deck[top_card_index - 1]) ): # check for pair 
+        #if (len(deck_game) >= 2 and (deck_game[top_card_index] == deck_game[top_card_index - 1]) ): # check for pair 
                 #is_pattern = True
 #
-        #if (len(deck) >= 3 and (deck[top_card_index] == deck[top_card_index - 2]) ): # check for sandiwch
+        #if (len(deck_game) >= 3 and (deck_game[top_card_index] == deck_game[top_card_index - 2]) ): # check for sandiwch
                 #is_pattern = True
+        
+        main_player.start()
 
-        if(current_player_id == 0):
-            player_thread(current_player_id, main_player_object)
-        current_player_id = (current_player_id + 1) %  len(game_players)
+        main_player.join()
+        current_player_id = (current_player_id + 1) %  len(players_game)
+        
 
-def player_thread(current_player_id, player_object):
-    print("Hello")
-    breakpoint()
+def control_player(current_player_id, player_object):
+    def slap():
+        pass
+
+    def place_card():
+        top_card_index = len(player_object.inventory) - 1
+        top_card = player_object.inventory.pop(top_card_index)
+        deck_lock.acquire()
+        deck_game.append(top_card)
+        deck_lock.release()
+
+    while(True):
+        player_input = input("(p)lace or (s)lap?: ") 
+        if (player_input in ['p', 'place']):
+            place_card()
+            break
+        if (player_input in ['s', 'slap']):
+            slap()
+            break
+        
+    
+
     if (current_player_id == 0):
         pass
 
